21f1d4e653f1b90ff9db9de08bcd1254dd436c49
manudevelopia <manugarciarodriguez@gmail.com>
2025-03-26 18:27:17 +0100

    records does not need annotation

--- src/test/java/info/developia/lib/dao/UserDao.java
+++ src/test/java/info/developia/lib/dao/UserDao.java
@@ -1,9 +1,7 @@
 package info.developia.lib.dao;
 
-import info.developia.lib.annotation.JsonProperty;
-
 public record UserDao(
-        @JsonProperty String name,
-        @JsonProperty int age
+        String name,
+        int age
 ) {
 }
