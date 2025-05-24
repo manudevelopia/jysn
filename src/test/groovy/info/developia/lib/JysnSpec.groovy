66b2be6248cfc73134be642586c3dbb312c0490c
manudevelopia <manugarciarodriguez@gmail.com>
2025-05-24 11:41:44 +0200

    remove type casting and unused test cases

--- src/test/groovy/info/developia/lib/JysnSpec.groovy
+++ src/test/groovy/info/developia/lib/JysnSpec.groovy
@@ -17,7 +17,7 @@ class JysnSpec extends Specification {
                 }
             }'''
         when:
-        def result = Jysn.from(json).to(User).parse() as User
+        def result = Jysn.from(json).to(User).parse()
         then:
         with(result) {
             name == 'John'
@@ -40,7 +40,7 @@ class JysnSpec extends Specification {
                 { "title": "1984" } 
             ]'''
         when:
-        def result = Jysn.from(json).to(Book).parse() as Book
+        def result = Jysn.from(json).to(Book).parse()
         then:
         result.size() == 2
         result[0].title == 'Lord of Rings'
@@ -61,143 +61,4 @@ class JysnSpec extends Specification {
         result.books[0].title == 'Lord of Rings 1'
         result.books[1].title == 'Lord of Rings 2'
     }
-
-//    def "should read json properties"() {
-//        given:
-//        String json = '''{
-//         "property1" : "value1",
-//         "property2" : "value2",
-//         "property3" : "value3",
-//         "number1" : 5.0,
-//         "number2" : 15,
-//         "boolean1" : true,
-//         "boolean2" : false,
-//         "nullable" : null,
-//         "object1" : {
-//             "property1" : "value1",
-//             "property2" : "value2",
-//             "property3object1" : {
-//                "property1" : "value1",
-//                "property2" : "value2"
-//             }
-//         }
-//        }'''
-//        when:
-//        def result = Jaysn.readProperties(json)
-//        then:
-//        readProperties.property1 == 'value1'
-//    }
-//
-//    def "should read json propertiess"() {
-//        given:
-//        String json = '''{
-//           "property1":"value1",
-//           "number1":5.0,
-//           "number2":15,
-//           "number3":-15,
-//           "boolean1":true,
-//           "nullable":null,
-//           "object":{
-//                "obj-prop1":"value1",
-//                "obj-prop2":"value2",
-//                "obj-obj1":{
-//                    "obj-prop1":"value1",
-//                    "obj-prop2":"value2"
-//                }
-//           },
-//           "boolean2":false,
-//           "boolean3":false,
-//           "array":[
-//               {
-//                    "array-prop1":"value1",
-//                    "array-prop2":"value2"
-//                }
-//           ]
-//        }'''
-//        when:
-//        Jaysn.parse(json, User)
-//        then:
-//        noExceptionThrown()
-//    }
-//
-//    def "should read json propertiess"() {
-//        given:
-//        String json = '''{
-//           "property1":"value1",
-//           "number1":5.0,
-//           "number2":15,
-//           "number3":-15,
-//           "boolean1":true,
-//           "nullable":null
-//        }'''
-//        when:
-//        Jaysn.parse(json, User)
-//        then:
-//        noExceptionThrown()
-//    }
-//
-//    def "should read nested object properties"() {
-//        given:
-//        String json = '''{
-//           "property1":"value1",
-//           "object":{
-//                "obj-prop1":"value1",
-//                "obj-obj1":{
-//                    "obj1-prop1":"value1",
-//                    "obj1-prop2":"value2"
-//                },
-//                "obj-prop2":"value2"
-//           }
-//           "property2":"value2",
-//        }'''
-//        when:
-//        Jaysn.parse(json, User)
-//        then:
-//        noExceptionThrown()
-//    }
-//
-//    def "should read objects array"() {
-//        given:
-//        String json = '''[
-//           {
-//               "object1-property1":"value1",
-//               "object1-property2":"value2",
-//           },
-//           {
-//               "object2-property1":"value1",
-//               "object2-property2":"value2",
-//           },
-//        ]'''
-//        when:
-//        def jsonNode = Jaysn.parse(json, User)
-//        then:
-//        noExceptionThrown()
-//    }
-//
-//    def "should read objects nested array"() {
-//        given:
-//        String json = '''[
-//           {
-//               "object1-property1":"value1",
-//               "object1-property2":"value2",
-//           },
-//           {
-//               "object2-array1": [
-//                     {
-//                          "array1-object1-property1":"value1",
-//                          "array1-object1-property2":"value2",
-//                     },
-//                     {
-//                          "array1-object2-property1":"value1",
-//                          "array1-object2-property2":"value2",
-//                     }
-//               ],
-//               "object2-property2":"value2",
-//           }
-//        ]'''
-//        when:
-//        def jsonNode = Jaysn.parse(json, User)
-//        then:
-//        noExceptionThrown()
-//    }
 }
