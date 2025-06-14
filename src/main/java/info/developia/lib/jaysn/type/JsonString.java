7b7acc58a15fae0b9e8cb8c3216dd22ac32c46bf
manudevelopia <manugarciarodriguez@gmail.com>
2025-06-14 13:41:08 +0200

    format style

--- src/main/java/info/developia/lib/jaysn/type/JsonString.java
+++ src/main/java/info/developia/lib/jaysn/type/JsonString.java
@@ -2,7 +2,10 @@ package info.developia.lib.jaysn.type;
 
 public class JsonString implements JsonValue {
     public final String value;
-    public JsonString(String value) { this.value = value; }
+
+    public JsonString(String value) {
+        this.value = value;
+    }
 
     @Override
     public String toString() {
