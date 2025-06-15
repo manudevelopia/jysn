d7c866fb7756f025e04636e1627dd273186d977e
manudevelopia <manugarciarodriguez@gmail.com>
2025-06-15 09:47:14 +0200

    depict benchmark test

--- src/jmh/java/info/developia/lib/Jaysn.java
+++ src/jmh/java/info/developia/lib/Jaysn.java
@@ -1,20 +1,34 @@
 package info.developia.lib;
 
+import info.developia.lib.jaysn.Jysn;
 import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
 import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.State;
 
-@State(Scope.Benchmark)
+import java.util.concurrent.TimeUnit;
+
+//@State(Scope.Benchmark)
+@State(Scope.Thread)
+@BenchmarkMode(Mode.Throughput)
+@OutputTimeUnit(TimeUnit.MILLISECONDS)
 public class Jaysn {
     private static final String TEXT = "Hello, world inside java!";
+    private final String json = """
+            {
+              "name": "John",
+              "age": 30,
+              "profile": {
+                 "status": "active",
+                           "roles": [ "admin", "user" ],
+                 "ids": [ 1, 2, 3 ]
+                }
+            }""";
 
     @Benchmark
-    public int benchmarkStringLength() {
-        return TEXT.length();
-    }
-
-    @Benchmark
-    public String benchmarkToUpperCase() {
-        return TEXT.toUpperCase();
+    public void benchmarkStringLength() {
+        Jysn.from(json).to(User.class);
     }
 }
